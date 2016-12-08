cd7c6ee0c02cb08846bc1b412f0b6dd4962aed16
caio <caiomateusgames@gmail.com>
2016-12-07 23:57:07 -0300

    JetAdmin

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 urlpatterns = [
    url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
     url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
-    url(r'^admin/', include(admin.site.urls)),
+ url(r'^admin/', include(admin.site.urls)),
    url(r'', include('blog.urls')),
  ]
